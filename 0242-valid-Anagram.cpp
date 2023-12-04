d7c3ba9ddce68d36e9075a493fd7f61a4d8e9c35
Srajana20 <136155358+Srajana20@users.noreply.github.com>
2023-12-04 18:34:42 +0530

    Create 0242-valid-Anagram.cpp

---
+++ 0242-valid-Anagram.cpp
@@ -0,0 +1,43 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int flag;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.size()==t.size()){
+            if(s==t){
+                flag=1;
+            }
+            else{
+                flag=0;
+            }
+
+        }
+        
+        if(flag==1)
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
